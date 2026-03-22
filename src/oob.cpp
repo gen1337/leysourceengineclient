88bf266c643f0f23df8f5cf4414d8e786e7e7fa9
genesis <142985074+gen1337@users.noreply.github.com>
2026-03-23 00:32:34 +0300

    Update oob.cpp

--- src/oob.cpp
+++ src/oob.cpp
@@ -100,7 +100,7 @@ bool OOB::ReceiveQueryPacketGetChallenge(
 
 	writeconnect.WriteString(nickname); //nick
 	writeconnect.WriteString(password); // pass
-	writeconnect.WriteString("2025.03.26"); // game version
+	writeconnect.WriteString("2026.03.16"); // game version
 
 
 	unsigned char steamkey[STEAM_KEYSIZE];
